8d4765b0efeb7459841474001154b5e0baa2ef88
Cristopher Yang <dg.cris.yang@gmail.com>
2026-04-03 15:45:34 -0300

    feat: write file

--- fileManagement/filesManagement.js
+++ fileManagement/filesManagement.js
@@ -8,3 +8,13 @@ export const getFileContent = async (path) => {
     throw error;
   }
 };
+
+export const writeFileContent = async (path, content) => {
+  try {
+    await fs.promises.writeFile(path, content, "utf-8");
+    console.log(`Archivo escrito exitosamente en ${path}`);
+  } catch (error) {
+    console.error(`Hubo un error al escribir el archivo: ${error.message}`);
+    throw error;
+  }
+};
--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-import { getFileContent } from "./fileManagement/filesManagement.js";
+import {
+  //   getFileContent,
+  writeFileContent,
+} from "./fileManagement/filesManagement.js";
 
-const content = getFileContent("./package.json");
-console.log(content);
+// const content = getFileContent("./package.json");
+// console.log(content);
+
+// ---------------------------
+
+const mockContent =
+  "Los alumnos deberán crear un archivo de JavaScript que realice consultas al API https://thronesapi.com/. Trabajar con métodos comunes y avanzados de los arrays, y utilizar el módulos File System de NodeJS. Cuando se solicite, persistir la información en un archivo local en formato JSON y realizar acciones sobre dicho archivo.";
+
+writeFileContent("./testOutput/mockContent.txt", mockContent);
